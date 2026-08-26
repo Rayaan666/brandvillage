24da0e67a8d256b4bf510921f8cf53727b864582
businessgpt02-code <businessgpt02@gmail.com>
2026-08-26 12:19:58 +0400

    s

--- src/data/heroSlides.js
+++ src/data/heroSlides.js
@@ -1,14 +1,14 @@
 export const heroSlides = [
   {
     id: 1,
-    image: "/home/1.png",
-    mobileImage: "https://res.cloudinary.com/n185h1km/image/upload/f_auto,q_auto/Untitled_design_92_b471cg",
+    image: "https://res.cloudinary.com/aqyf2pjl/image/upload/f_auto,q_auto/Untitled_design_-_2026-08-26T092026.185",
+    mobileImage: "https://res.cloudinary.com/aqyf2pjl/image/upload/f_auto,q_auto/Untitled_design_92_b471cg",
     imageAlt: "Brand Village Outlet"
   },
   {
     id: 2,
-    image: "/home/2.png",
-    mobileImage: "https://res.cloudinary.com/n185h1km/image/upload/f_auto,q_auto/Untitled_design_93_lz3qer",
+    image: "https://res.cloudinary.com/aqyf2pjl/image/upload/f_auto,q_auto/Untitled_design_-_2026-08-26T092157.684",
+    mobileImage: "https://res.cloudinary.com/aqyf2pjl/image/upload/f_auto,q_auto/Untitled_design_93_lz3qer",
     imageAlt: "Brand Village Outlet"
   }
 ];
